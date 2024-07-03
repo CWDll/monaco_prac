e3ebc812ca34eeb05072b19d8fa2ac7827226cf7
이창준 <2018110021@dongguk.edu>
2024-07-03 16:49:22 +0900

    [Add] 언어에 따른 기본 코드 스니펫 추가

--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,12 @@ export const LANGUAGE_VERSION: { [key: string]: string } = {
   csharp: "6.12.0",
   php: "8.2.3",
 };
+
+export const CODE_SNIPPETS = {
+  javascript: `function greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\n\ngreet("Alex");\n`,
+  typescript: `type Params = {\n\tname: string;\n};\n\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Alex" });\n`,
+  python: `def greet(name):\n\tprint("Hello, " + name + "!")\n\n\ngreet("Alex")\n`,
+  java: `public class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
+  csharp: `using System;\n\nnamespace HelloWorld {\n\tclass Hello {\n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n`,
+  php: `<?php\n$name = 'Alex';\necho $name;\n?>\n`,
+};
