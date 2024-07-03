9c71ea2bb62a77d234d2ab4607ab06f75c3fd080
이창준 <2018110021@dongguk.edu>
2024-07-03 15:23:15 +0900

    [Feat] mount시 editor에 focus

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,8 +1,17 @@
-import { Box, chakra } from "@chakra-ui/react";
-import { Editor } from "@monaco-editor/react";
-import React, { useState } from "react";
+import { Box } from "@chakra-ui/react";
+import { Editor, OnMount } from "@monaco-editor/react";
+import React, { useState, useRef } from "react";
+import * as monaco from "monaco-editor";
 
 const CodeEditor: React.FC = () => {
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+  const [value, setValue] = useState<string>();
+
+  const handleEditorMount: OnMount = (editor) => {
+    editorRef.current = editor;
+    editor.focus();
+  };
+
   return (
     <Box>
       <Editor
@@ -10,6 +19,9 @@ const CodeEditor: React.FC = () => {
         theme="vs-dark"
         defaultLanguage="javascript"
         defaultValue="// some comment"
+        onMount={handleEditorMount}
+        value={value}
+        onChange={(val) => setValue(val || "")}
       />
     </Box>
   );
