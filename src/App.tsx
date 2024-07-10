c01ecfbbc8a775dcd56fd15afdc280df567409b0
이창준 <2018110021@dongguk.edu>
2024-07-10 13:30:33 +0900

    [Style] page 구성 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,37 @@ import { Box, Button, Flex } from "@chakra-ui/react";
 import { Routes, Route, Link } from "react-router-dom";
 import CodeEditor from "./pages/CodeEditor";
 import TADiffEditPage from "./pages/TADiffEditPage";
+import Home from "./pages/Home";
+import TAEditPage from "./pages/TAEditPage";
 
 function App() {
   return (
     <Box minH="100vh" bg="#0f0a19" color="gray.500">
+      <Box>sd</Box>
       <Flex justifyContent="center" py={4}>
         <Link to="/code-editor">
           <Button mx={2} colorScheme="teal">
             Code Editor
           </Button>
         </Link>
-        <Link to="/ta-page">
+        <p>/</p>
+        <Link to="/ta-diffEditPage">
           <Button mx={2} colorScheme="teal">
-            TA Page
+            TA Diff Edit Page
+          </Button>
+        </Link>
+        <p>/</p>
+        <Link to="/ta-editPage">
+          <Button mx={2} colorScheme="teal">
+            TA Edit Page
           </Button>
         </Link>
       </Flex>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/code-editor" element={<CodeEditor />} />
-        <Route path="/ta-page" element={<TADiffEditPage />} />
+        <Route path="/ta-diffEditPage" element={<TADiffEditPage />} />
+        <Route path="/ta-editPage" element={<TAEditPage />} />
       </Routes>
     </Box>
   );
---
+++ src/pages/TAEditPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TAEditPage = () => {
+  return <div>TAEditPage</div>;
+};
+
+export default TAEditPage;
