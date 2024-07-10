acdf476e0c6c48f521856458a96b6832c6633c0b
이창준 <2018110021@dongguk.edu>
2024-07-10 17:00:22 +0900

    [Refactor] 선택된 파일 색 변경 코드를 간결화한다.

--- src/pages/TAEditPage.tsx
+++ src/pages/TAEditPage.tsx
@@ -9,34 +9,28 @@ const TAEditPage = () => {
   const [fileName, setFileName] = useState<keyof FilesType>("script.js");
   const file: FileType = files[fileName];
 
+  const getColor = (currentFileName: keyof FilesType) => {
+    return fileName === currentFileName ? "teal.500" : "gray.200";
+  };
+
+  const renderListItem = (currentFileName: keyof FilesType) => (
+    <ListItem color={getColor(currentFileName)} key={currentFileName}>
+      <button
+        disabled={fileName === currentFileName}
+        onClick={() => setFileName(currentFileName)}
+      >
+        {currentFileName}
+      </button>
+    </ListItem>
+  );
+
   return (
     <Flex direction="row" padding={0}>
       <Flex direction="column" padding={6} pt={0} pb={0}>
         <UnorderedList>
-          <ListItem color={fileName === "script.js" ? "teal.500" : "grey.200"}>
-            <button
-              disabled={fileName === "script.js"}
-              onClick={() => setFileName("script.js")}
-            >
-              script.js
-            </button>
-          </ListItem>
-          <ListItem color={fileName === "style.css" ? "teal.500" : "grey.200"}>
-            <button
-              disabled={fileName === "style.css"}
-              onClick={() => setFileName("style.css")}
-            >
-              style.css
-            </button>
-          </ListItem>
-          <ListItem color={fileName === "index.html" ? "teal.500" : "grey.200"}>
-            <button
-              disabled={fileName === "index.html"}
-              onClick={() => setFileName("index.html")}
-            >
-              index.html
-            </button>
-          </ListItem>
+          {renderListItem("script.js")}
+          {renderListItem("style.css")}
+          {renderListItem("index.html")}
         </UnorderedList>
       </Flex>
       <MultiModelEditor selectedFile={file} />
