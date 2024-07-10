744c4639ec0cb6004f033ea6a390e03fdc2293c8
이창준 <2018110021@dongguk.edu>
2024-07-10 15:43:15 +0900

    [Feat] Multi Model에서 리스트, 코드실행 부분 분리

--- src/components/MultiModelEditor.tsx
+++ src/components/MultiModelEditor.tsx
@@ -3,51 +3,23 @@ import Editor from "@monaco-editor/react";
 import files, { FileType, FilesType } from "../files";
 import { Flex, UnorderedList, ListItem } from "@chakra-ui/react";
 
-function MultiModelEditor() {
-  const [fileName, setFileName] = useState<keyof FilesType>("script.js");
-
-  const file: FileType = files[fileName];
-
-  return (
-    <>
-      <Flex direction="column" padding={6} pt={0} pb={0}>
-        <UnorderedList>
-          <ListItem>
-            <button
-              disabled={fileName === "script.js"}
-              onClick={() => setFileName("script.js")}
-            >
-              script.js
-            </button>
-          </ListItem>
-          <ListItem>
-            <button
-              disabled={fileName === "style.css"}
-              onClick={() => setFileName("style.css")}
-            >
-              style.css
-            </button>
-          </ListItem>
-          <ListItem>
-            <button
-              disabled={fileName === "index.html"}
-              onClick={() => setFileName("index.html")}
-            >
-              index.html
-            </button>
-          </ListItem>
-        </UnorderedList>
-      </Flex>
-      <Editor
-        height="90vh"
-        width="70vw"
-        theme="vs-dark"
-        path={file.name}
-        defaultLanguage={file.language}
-        defaultValue={file.value}
-      />
-    </>
-  );
+interface MultiModelEditorProps {
+  selectedFile: FileType;
 }
+
+const MultiModelEditor: React.FC<MultiModelEditorProps> = ({
+  selectedFile,
+}) => {
+  return (
+    <Editor
+      height="90vh"
+      width="60vw"
+      theme="vs-dark"
+      path={selectedFile.name}
+      defaultLanguage={selectedFile.language}
+      defaultValue={selectedFile.value}
+    />
+  );
+};
 
 export default MultiModelEditor;
--- src/pages/TAEditPage.tsx
+++ src/pages/TAEditPage.tsx
@@ -1,11 +1,44 @@
-import { Flex } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
+import { Flex, UnorderedList, ListItem } from "@chakra-ui/react";
+
+import files, { FileType, FilesType } from "../files";
 import MultiModelEditor from "../components/MultiModelEditor";
 
 const TAEditPage = () => {
+  const [fileName, setFileName] = useState<keyof FilesType>("script.js");
+  const file: FileType = files[fileName];
+
   return (
     <Flex direction="row" padding={0}>
-      <MultiModelEditor />
+      <Flex direction="column" padding={6} pt={0} pb={0}>
+        <UnorderedList>
+          <ListItem>
+            <button
+              disabled={fileName === "script.js"}
+              onClick={() => setFileName("script.js")}
+            >
+              script.js
+            </button>
+          </ListItem>
+          <ListItem>
+            <button
+              disabled={fileName === "style.css"}
+              onClick={() => setFileName("style.css")}
+            >
+              style.css
+            </button>
+          </ListItem>
+          <ListItem>
+            <button
+              disabled={fileName === "index.html"}
+              onClick={() => setFileName("index.html")}
+            >
+              index.html
+            </button>
+          </ListItem>
+        </UnorderedList>
+      </Flex>
+      <MultiModelEditor selectedFile={file} />
     </Flex>
   );
 };
