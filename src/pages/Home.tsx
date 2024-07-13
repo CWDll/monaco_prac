d53fd183e26ce520965c21fffc8bbae8e725f683
이창준 <2018110021@dongguk.edu>
2024-07-13 22:01:04 +0900

    [Feat] intro 글자 나타내는 애니메이션 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "../components/widgets/Containert";
 import Column from "../components/widgets/Column";
 import Row from "../components/widgets/Row";
@@ -7,25 +7,93 @@ import SizedBox from "../components/widgets/SizedBox";
 import Stack from "../components/widgets/Stack";
 
 const Home: React.FC = () => {
+  const [introText, setIntroText] = React.useState<string>("");
+  const fullIntroText = "플러터 위젯 테스트중 !";
+
+  useEffect(() => {
+    let count = 0;
+    const interval = setInterval(() => {
+      if (count === fullIntroText.length - 1) {
+        count = 0;
+        setIntroText("");
+      } else {
+        setIntroText((prev) => prev + fullIntroText[count]);
+        ++count;
+      }
+    }, 200);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
-      <h1>Flutter Widgets in React</h1>
-      <Container width="200px" height="200px" backgroundColor="lightblue">
-        <Column>
-          <Row>
-            <div>Row Item 1</div>
-            <div>Row Item 2</div>
-          </Row>
-          <SizedBox height="20px" />
-          <div>Column Item 1</div>
+      <h1>{introText}</h1>
+
+      <h2>Column Example</h2>
+      <Container
+        width="200px"
+        height="200px"
+        backgroundColor="lightblue"
+        borderRadius="5%"
+      >
+        <Column justifyContent="center" alignItems="center">
+          <div>Item 1</div>
+          <div>Item 2</div>
         </Column>
       </Container>
-      <Stack>
-        <div>Stack Item 1</div>
-        <div>Stack Item 2</div>
-        <div>Stack Item 3</div>
-        <div>Stack Item 4</div>
-      </Stack>
+
+      <h2>Row Example</h2>
+      <Container
+        width="200px"
+        height="200px"
+        backgroundColor="lightgreen"
+        boxShadow="5px 2px 2px white"
+      >
+        <Row justifyContent="space-between" alignItems="center">
+          <div>Item 1</div>
+          <div>Item 2</div>
+        </Row>
+      </Container>
+
+      <h2>SizedBox Example</h2>
+      <Container width="20vw" height="20vh" backgroundColor="lightcoral">
+        <div>Above</div>
+        <SizedBox height="20px" />
+        <div>Below</div>
+      </Container>
+
+      <h2>Stack Example</h2>
+      <Container width="200px" height="200px" backgroundColor="lightgrey">
+        <Stack>
+          <div
+            style={{
+              background: "rgba(255, 0, 0, 0.5)",
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            Layer 1
+          </div>
+          <div
+            style={{
+              background: "rgba(0, 255, 0, 0.5)",
+              width: "80%",
+              height: "80%",
+            }}
+          >
+            Layer 2
+          </div>
+          <div
+            style={{
+              background: "rgba(0, 0, 255, 0.5)",
+              width: "60%",
+              height: "60%",
+            }}
+          >
+            Layer 3
+          </div>
+        </Stack>
+      </Container>
     </div>
   );
 };
