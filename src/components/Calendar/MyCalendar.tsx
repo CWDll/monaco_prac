7ea3cd9a3cdc53f78f26ec456b9de28b3b255429
이창준 <2018110021@dongguk.edu>
2024-07-28 20:26:07 +0900

    [Feat] 원하는 날짜 타일에 태그를 삽입한다.

--- src/components/Calendar/MyCalendar.tsx
+++ src/components/Calendar/MyCalendar.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import * as S from "./style";
 import { CalendarProps } from "react-calendar";
+import moment from "moment";
 
 const MyCalendar = () => {
   // useState 훅의 초기값으로 현재 날짜를 넣어줌
@@ -24,6 +25,28 @@ const MyCalendar = () => {
     return null;
   };
 
+  // 태그 삽입 날짜 리스트
+  const dayList = [
+    "2024-07-02",
+    "2024-07-05",
+    "2024-07-08",
+    "2024-07-11",
+    "2024-07-14",
+    "2024-07-17",
+    "2024-07-20",
+  ];
+  // 태그 삽입 날짜 타일에 컨텐츠 추가
+  const addTag = ({ date }: any) => {
+    // 해당 날짜(하루)에 추가할 컨텐츠의 배열
+    const contents: any[] = [];
+
+    // date가 dayList와 일치하면 태그 추가 (npm install moment)
+    if (dayList.find((day) => day === moment(date).format("YYYY-MM-DD"))) {
+      contents.push(<span className="tag">태그</span>);
+    }
+    return <div>{contents}</div>;
+  };
+
   return (
     <S.CalendarBox>
       <S.StyleCalendar
@@ -38,7 +61,7 @@ const MyCalendar = () => {
         // 이전 달, 다음 달 날짜 숨기기
         showNeighboringMonth={false}
         // 오늘 날짜를 "오눌"로 표시
-        tileContent={tileContent}
+        tileContent={addTag}
       />
     </S.CalendarBox>
   );
